147b78c19791cd65ed85875c6f6347882b414244
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-12-08 23:05:51 +0530

    patch

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,48 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Grid from '@mui/material/Grid'
-import TextField from '@mui/material/TextField';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import "./styles/Events.css";
 import Event from "./Event";
 import data from "../data/events.json";
 import { Button, Typography } from "@mui/material";
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
-
-
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 function Events() {
-  const[ eventsCard , setEventsCards] = useState(data);
-  const [btnText , setBtnText] = useState("All");
-  const filterCard = (parameter)=>{
-     const filteredCard = data.filter(card => card.day === parameter);
-     if(parameter != null){
-      setEventsCards(filteredCard)
-     }
-     else{
+  const [userFilter, setUserFilter] = useState(data);
+  const [eventsCard, setEventsCards] = useState(data);
+  const [dayfilter , setDayFilter] = useState('all');
+  const [btnText, setBtnText] = useState("All");
+  const [opn, setOpn] = React.useState(false);
+
+  
+
+  useEffect(()=>{
+    if(dayfilter === 'all') {
+       setUserFilter(eventsCard) 
+       setAnchorEl(null);
+       setBtnText(dayfilter)
+       return;
+       }
+    const filtered = eventsCard.filter((ca) => ca.day === dayfilter );
+    setUserFilter(filtered);
+    setAnchorEl(null);
+    setBtnText(dayfilter)
+  }, [dayfilter])
+
+
+ 
+
+  const filterCard = (parameter) => {
+    const filteredCard = data.filter((card) => card.day === parameter);
+    if (parameter != null) {
+      setEventsCards(filteredCard);
+    } else {
       setEventsCards(data);
-     }
-  }
+    }
+  };
 
   const [anchorEl, setAnchorEl] = React.useState(null);
+
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+    setAnchorEl(event.currentTarget)
+    setOpn((prev) => !prev);
   };
+  const handleClickAway = () => {
+    setOpn(false);
+  };
+
   const handleClose = (parameter) => {
-    if(parameter != null) setBtnText(parameter) ; else setBtnText("all");
+    if (parameter != null) setBtnText(parameter);
+    else{ setBtnText("all") }
     filterCard(parameter);
     setAnchorEl(null);
   };
-  
+
 
   return (
     <div className="body">
@@ -55,38 +81,45 @@ function Events() {
       </div>
 
       <div className="hero">
-        <img src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGNvbmNlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+        <img
+          src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGNvbmNlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt=""
+        />
       </div>
 
       <div className="search">
         <div className="search-inputs">
           <div className="search-field border-left">
             <h3>Looking for</h3>
-            <TextField fullWidth sx={{ input: { color: 'white' } }}
+            <TextField
+              fullWidth
+              sx={{ input: { color: "white" } }}
               variant="standard"
               color="secondary"
               focused
               defaultValue="Webinar"
             />
           </div>
+
           <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={()=> handleClose()}>All</MenuItem>
-        <MenuItem onClick={()=> handleClose("weekday")}>Weekdays</MenuItem>
-        <MenuItem onClick={()=> handleClose("weekend")}>Weekend</MenuItem>
-    
-      </Menu>
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={() => setDayFilter('all') }>All</MenuItem>
+            <MenuItem onClick={() => setDayFilter('weekday') }>Weekdays</MenuItem>
+            <MenuItem onClick={() => setDayFilter('weekend') }>Weekend</MenuItem>
+          </Menu>
 
           <div className="search-field border-left">
             <h3>In</h3>
-            <TextField fullWidth sx={{ input: { color: 'white' } }}
+            <TextField
+              fullWidth
+              sx={{ input: { color: "white" } }}
               variant="standard"
               color="secondary"
               focused
@@ -95,13 +128,14 @@ function Events() {
           </div>
           <div className="search-field">
             <h3>When</h3>
-            <TextField fullWidth sx={{ input: { color: 'white' } }}
+            <TextField
+              fullWidth
+              sx={{ input: { color: "white" } }}
               variant="standard"
               color="secondary"
               focused
               defaultValue="Any date"
             />
-
           </div>
         </div>
         <div className="search-all">
@@ -110,40 +144,62 @@ function Events() {
       </div>
 
       <div className="filters">
-      <Grid display='flex' justifyContent="space-between">
-        <h1>Upcoming events</h1>
-        <Grid display="flex" alignItems="center"  spacing={2} width={400} justifyContent="space-between">
-            <Grid item >
-            <Button   
-             
-            disableRipple disableElevation 
-            endIcon={<KeyboardArrowDownIcon color="secondary" />}
-             color="error"
-             id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                 aria-haspopup="true"
-               aria-expanded={open ? 'true' : undefined}
-               onClick={handleClick}
-             variant="contained"> {btnText} </Button>
-          
+        <Grid display="flex" justifyContent="space-between">
+          <h1>Upcoming events</h1>
+          <Grid
+            display="flex"
+            alignItems="center"
+            spacing={2}
+            width={400}
+            justifyContent="space-between"
+          >
+            <Grid item>
+              <Button
+                disableRipple
+                disableElevation
+                endIcon={<KeyboardArrowDownIcon color="secondary" />}
+                color="error"
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                variant="contained"
+              >
+                {" "}
+                {btnText}{" "}
+              </Button>
             </Grid>
-            <Grid item >
-            <Button disableElevation  endIcon={<KeyboardArrowDownIcon color="secondary"/>}  color="error" variant="contained"> Event Type </Button>
+            <Grid item>
+              <Button
+                disableElevation
+                endIcon={<KeyboardArrowDownIcon color="secondary" />}
+                color="error"
+                variant="contained"
+              >
+                {" "}
+                Event Type{" "}
+              </Button>
             </Grid>
-            <Grid item >
-            <Button disableElevation endIcon={<KeyboardArrowDownIcon color="secondary"/>}   color="error" variant="contained"> Category </Button>
+            <Grid item>
+              <Button
+                disableElevation
+                endIcon={<KeyboardArrowDownIcon color="secondary" />}
+                color="error"
+                variant="contained"
+              >
+                {" "}
+                Category{" "}
+              </Button>
             </Grid>
           </Grid>
-      </Grid>
-       
-        <div className="filter">
+        </Grid>
 
-        </div>
+        <div className="filter"></div>
       </div>
 
-      
       <div className="events">
-        {eventsCard.map((card) => {
+        {userFilter.map((card) => {
           return (
             <Event
               key={card.id}
